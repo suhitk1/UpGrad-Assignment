e96621d629c1886166ad706a2ef95b05fa757cf9
SUHIT KUMAR <suhit.kmr1@gmail.com>
2019-03-26 14:45:07 +0530

    Update make_join2data.py

--- make_join2data.py
+++ make_join2data.py
@@ -1,18 +1,11 @@
-#!/usr/bin/env python
 import sys
-
-# --------------------------------------------------------------------------
-#  (make_join2data.py) Generate a random combination of titles and viewer counts, or channels
-# this is a simple version of a congruential generator, 
-#   not a great random generator but enough  
-# --------------------------------------------------------------------------
 
 chans   = ['ABC','DEF','CNO','NOX','YES','CAB','BAT','MAN','ZOO','XYZ','BOB']
 sh1 =['Hot','Almost','Hourly','PostModern','Baked','Dumb','Cold','Surreal','Loud']
 sh2 =['News','Show','Cooking','Sports','Games','Talking','Talking']
 vwr =range(17,1053)
 
-chvnm=sys.argv[1]  #get number argument, if its n, do numbers not channels,
+chvnm=sys.argv[1]  
 
 lch=len(chans)
 lsh1=len(sh1)
